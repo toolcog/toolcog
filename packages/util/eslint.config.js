c5aef4995777b3beeda3db98c0f41da8093d6d38
Chris Sachs <chris@toolcog.com>
2024-10-08 08:18:14 -0700

    chore: Flatten packages directory
    The grouping by framework, plugin, and adapter packages was unnecessary
    and getting annoying. I don't think such a grouping will be desirable
    even as the number of plugins and adapters grow. There are more appropriate
    places to convey the distinction.

---
+++ packages/util/eslint.config.js
@@ -0,0 +1,11 @@
+import config from "../../eslint.config.js";
+
+export default [
+  ...config,
+  {
+    ignores: [
+      "src/emit/emitter.ts", // crashes typescript-eslint 8.6
+      "src/tty/width.ts", // takes an eternity to lint
+    ],
+  },
+];
